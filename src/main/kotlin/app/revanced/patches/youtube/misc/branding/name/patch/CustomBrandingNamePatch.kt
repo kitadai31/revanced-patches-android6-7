2ad8c2400ce6005a4e486d1d4c5f70d68270c225
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2023-08-25 16:59:37 +0900

    fix(custom-branding-name): improve patching speed

--- src/main/kotlin/app/revanced/patches/youtube/misc/branding/name/patch/CustomBrandingNamePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/branding/name/patch/CustomBrandingNamePatch.kt
@@ -3,17 +3,15 @@ package app.revanced.patches.youtube.misc.branding.name.patch
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.data.ResourceContext
-import app.revanced.patcher.patch.annotations.DependsOn
-import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
 import app.revanced.shared.annotation.YouTubeCompatibility
-import app.revanced.shared.extensions.startsWithAny
 import app.revanced.shared.patches.options.PatchOptions
 import app.revanced.shared.util.resources.ResourceHelper
-import org.w3c.dom.Element
 
 @Patch
 @Name("custom-branding-name")
@@ -29,28 +27,16 @@ class CustomBrandingNamePatch : ResourcePatch {
     override fun execute(context: ResourceContext): PatchResult {
 
         // App name
-        val resourceFileNames = arrayOf("strings.xml")
         val appName = PatchOptions.YouTube_AppName
 
-        context.forEach {
-            if (!it.name.startsWithAny(*resourceFileNames)) return@forEach
-
-            // for each file in the "layouts" directory replace all necessary attributes content
-            context.xmlEditor[it.absolutePath].use { editor ->
-            val resourcesNode = editor.file.getElementsByTagName("resources").item(0) as Element
-
-                for (i in 0 until resourcesNode.childNodes.length) {
-                    val node = resourcesNode.childNodes.item(i)
-                    if (node !is Element) continue
-
-                    val element = resourcesNode.childNodes.item(i) as Element
-                    element.textContent = when (element.getAttribute("name")) {
-                        "application_name" -> "$appName"
-                        else -> continue
-                    }
-                }
-            }
-        }
+        val manifest = context["AndroidManifest.xml"]
+        manifest.writeText(
+            manifest.readText()
+                .replace(
+                    "android:label=\"@string/application_name",
+                    "android:label=\"$appName"
+                )
+        )
 
         ResourceHelper.labelPatchSuccess(
             context,
