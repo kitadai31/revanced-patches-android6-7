c0798b8bc8e704916e7aaa319630b7e9c5da6acc
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2023-08-29 20:39:55 +0900

    Remove `hide-shorts-navbar` patch
    Since spoofing app version to 17.40.xx+ is broken, this patch no longer works.

--- src/main/kotlin/app/revanced/patches/youtube/layout/general/pivotbar/shortspivotbar/bytecode/fingerprints/ReelWatchEndpointFingerprint.kt
+++
@@ -1,8 +0,0 @@
-package app.revanced.patches.youtube.layout.general.pivotbar.shortspivotbar.bytecode.fingerprints
-
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
-object ReelWatchEndpointFingerprint : MethodFingerprint(
-    returnType = "V",
-    customFingerprint = { methodDef, _ -> methodDef.name == "<init>" }
-)
