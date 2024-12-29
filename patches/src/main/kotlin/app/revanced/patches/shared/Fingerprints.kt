fdf61dc1416f998124226c61d98afe1d17a86e48
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2024-12-29 19:08:21 +0900

    Fix SpoofStreamingDataPatch

--- patches/src/main/kotlin/app/revanced/patches/shared/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/Fingerprints.kt
@@ -54,7 +54,7 @@ internal val formatStreamModelConstructorFingerprint = legacyFingerprint(
     accessFlags = AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR,
     opcodes = listOf(
         Opcode.IGET_WIDE,
-        Opcode.IPUT_WIDE,
+        Opcode.NEW_INSTANCE,
     ),
     literals = listOf(45374643L),
 )
