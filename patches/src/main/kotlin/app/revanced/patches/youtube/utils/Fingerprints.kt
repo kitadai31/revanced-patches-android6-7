52e0e90c1bd31a7f8ba994ff7d9c0fcf0b3c8e54
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2024-12-31 11:10:20 +0900

    Fix PlayerComponentsPatch
    * Temporary removed hide seek message: I couldn't fix the crash

--- patches/src/main/kotlin/app/revanced/patches/youtube/utils/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/utils/Fingerprints.kt
@@ -247,7 +247,6 @@ internal val youtubeControlsOverlayFingerprint = legacyFingerprint(
     parameters = emptyList(),
     literals = listOf(
         fadeDurationFast,
-        insetOverlayViewLayout,
         scrimOverlay,
         // Removed in YouTube 20.02.38+
         // seekUndoEduOverlayStub
