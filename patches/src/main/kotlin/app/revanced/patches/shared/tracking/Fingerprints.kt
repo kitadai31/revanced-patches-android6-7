84eca85f87b21fbf8d034f4746d5a218e0d090d5
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2025-01-01 20:29:06 +0900

    Fix SanitizeUrlQueryPatch

--- patches/src/main/kotlin/app/revanced/patches/shared/tracking/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/tracking/Fingerprints.kt
@@ -12,17 +12,6 @@ internal val copyTextEndpointFingerprint = legacyFingerprint(
     name = "copyTextEndpointFingerprint",
     returnType = "V",
     parameters = listOf("L", "Ljava/util/Map;"),
-    opcodes = listOf(
-        Opcode.IGET_OBJECT,
-        Opcode.CONST_STRING,
-        Opcode.INVOKE_STATIC,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.IGET_OBJECT,
-        Opcode.IGET_OBJECT,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.RETURN_VOID
-    ),
     strings = listOf("text/plain")
 )
 
