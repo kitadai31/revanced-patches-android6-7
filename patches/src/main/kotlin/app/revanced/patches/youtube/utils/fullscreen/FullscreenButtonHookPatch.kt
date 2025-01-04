03f4fb6caec51dc2374fe4c27aa8f61e0553335f
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2025-01-04 14:10:06 +0900

    Fix FullscreenButtonHookPatch

--- patches/src/main/kotlin/app/revanced/patches/youtube/utils/fullscreen/FullscreenButtonHookPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/utils/fullscreen/FullscreenButtonHookPatch.kt
@@ -43,7 +43,7 @@ val fullscreenButtonHookPatch = bytecodePatch(
             nextGenWatchLayoutFullscreenModeFingerprint.methodOrThrow().apply {
                 val methodIndex = indexOfFirstInstructionReversedOrThrow {
                     opcode == Opcode.INVOKE_DIRECT &&
-                            getReference<MethodReference>()?.parameterTypes?.size == 2
+                            getReference<MethodReference>()?.parameterTypes?.size == 3
                 }
                 val fieldIndex =
                     indexOfFirstInstructionReversedOrThrow(methodIndex, Opcode.IGET_OBJECT)
@@ -68,9 +68,9 @@ val fullscreenButtonHookPatch = bytecodePatch(
                     }
                 } else {
                     val animatorListenerClass =
-                        (getInstruction<ReferenceInstruction>(methodIndex).reference as MethodReference).definingClass
+                        "Lkqn;"
                     return Pair(
-                        findMethodOrThrow(animatorListenerClass) { parameters == listOf("I") },
+                        findMethodOrThrow(animatorListenerClass) { name == "a" },
                         fullscreenActionClass
                     )
                 }
