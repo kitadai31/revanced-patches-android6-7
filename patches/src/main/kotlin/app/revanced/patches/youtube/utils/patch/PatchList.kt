ebd40dc249efb1509accaeb5f5a74165e60ceeb9
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2025-01-05 15:32:12 +0900

    feat: Add `Shorts seek` patch

--- patches/src/main/kotlin/app/revanced/patches/youtube/utils/patch/PatchList.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/utils/patch/PatchList.kt
@@ -221,6 +221,10 @@ internal enum class PatchList(
         "Shorts components",
         "Adds options to hide or change components related to YouTube Shorts."
     ),
+    SHORTS_SEEK(
+        "Shorts seek",
+        "Adds an option to replace toolbar buttons in Shorts player with skip/rewind buttons."
+    ),
     SNACK_BAR_COMPONENTS(
         "Snack bar components",
         "Adds options to hide or change components related to the snack bar."
