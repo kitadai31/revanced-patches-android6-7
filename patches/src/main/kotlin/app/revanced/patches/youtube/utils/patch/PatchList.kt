b6736823cd4e304e9c56b393bd59a79f3867d3c7
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2025-05-13 18:31:13 +0900

    feat: Add `Add missing resources` patch

--- patches/src/main/kotlin/app/revanced/patches/youtube/utils/patch/PatchList.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/utils/patch/PatchList.kt
@@ -284,5 +284,9 @@ internal enum class PatchList(
     FREEZE_LAYOUT_UPDATES(
         "Freeze layout updates",
         "Freeze the current server-side layout. Mandatory for 17.34.36."
+    ),
+    ADD_MISSING_RESOURCES(
+        "Add missing resources",
+        "Add missing resources to prevent crashes in 17.34.36. Currently, only a few resources are supported. Also, replace missing drawables with a transparent image to prevent crashes."
     )
 }
